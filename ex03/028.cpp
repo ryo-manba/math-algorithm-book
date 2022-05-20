027c75437fb3568b411d560d0408fb5d06cd200c
Ryomanba <datemanba3@gmail.com>
2022-05-20 22:48:10 +0900

    3.7 節｜動的計画法　～漸化式の利用～

---
+++ ex03/028.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#define rep1(n)       for (int rep1_i= 0; rep1_i < n; rep1_i++)
+#define rep2(i, n)    for (int i = 0; i < n; i++)
+#define rep3(i, m, n) for (int i = m; i < n; i++)
+#define overload3(o1, o2, o3, name, ...) name
+#define rep(...) overload3(__VA_ARGS__, rep3,rep2,rep1)(__VA_ARGS__)
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+struct fast_ios { fast_ios(){ cin.tie(nullptr), ios::sync_with_stdio(false), cout << fixed << setprecision(20); }; } fast_ios_;
+const int INF = (int)1e9;
+const ll INFL = (ll)1e18;
+const int MOD = 1e9 + 7;
+const double EPS = 1e-10;
+int dx[]={0, 0, -1, 1};
+int dy[]={1, -1, 0, 0};
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+#define debug(var)  do{std::cout << #var << " : ";view(var);}while(0)
+template<typename T> void view(T e){std::cout << e << std::endl;}
+template<typename T> void view(const std::vector<T>& v){for(const auto& e : v){ std::cout << e << " "; } std::cout << std::endl;}
+template<typename T> void view(const std::vector<std::vector<T> >& vv){ for(const auto& v : vv){ view(v); } }
+
+int dp[100100];
+
+int main()
+{
+	int n;
+	cin >> n;
+	vector<int> h(n);
+	rep(i,n) cin >> h[i];
+
+	int sum = 0;
+	rep(i,1,n)
+	{
+		if (i == 1) dp[i] = abs(h[i - 1] - h[i]);
+		else
+		{
+			int v1 = dp[i - 1] + abs(h[i] - h[i - 1]);
+			int v2 = dp[i - 2] + abs(h[i] - h[i - 2]);
+			dp[i] = min(v1, v2);
+		}
+	}
+	cout << dp[n - 1] << endl;
+	return 0;
+}
