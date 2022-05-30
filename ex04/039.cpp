bcf8ea980389dde25a09202a45d0543c7b748157
Ryomanba <datemanba3@gmail.com>
2022-05-31 01:07:55 +0900

    いもす法

---
+++ ex04/039.cpp
@@ -0,0 +1,54 @@
+// #pragma GCC target ("avx2")
+// #pragma GCC optimization ("O3")
+// #pragma GCC optimization ("unroll-loops")
+#include <bits/stdc++.h>
+#define rep1(n)       for (int rep1_i= 0; rep1_i < n; rep1_i++)
+#define rep2(i, n)    for (int i = 0; i < n; i++)
+#define rep3(i, m, n) for (int i = m; i <= n; i++)
+#define overload3(o1, o2, o3, name, ...) name
+#define rep(...) overload3(__VA_ARGS__, rep3,rep2,rep1)(__VA_ARGS__)
+#define INF ((1<<30)-1)
+#define INFL ((1LL<<62)-1)
+#define MOD 998244353
+using namespace std;
+using ll = long long;
+using ld = long double;
+using pll = pair<ll, ll>;
+struct fast_ios { fast_ios(){ cin.tie(nullptr), ios::sync_with_stdio(false), cout << fixed << setprecision(20); }; } fast_ios_;
+int dx[]={0, 0, -1, 1};
+int dy[]={1, -1, 0, 0};
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+#define debug(var)  do{std::cout << #var << " : ";view(var);}while(0)
+template<typename T> void view(T e){std::cout << e << std::endl;}
+template<typename T> void view(const std::vector<T>& v){for(const auto& e : v){ std::cout << e << " "; } std::cout << std::endl;}
+template<typename T> void view(const std::vector<std::vector<T> >& vv){ for(const auto& v : vv){ view(v); } }
+
+int v[100010];
+
+int main()
+{
+	int n, q;
+	cin >> n >> q;
+	vector<int> l(q), r(q), x(q);
+	rep(i,q) cin >> l[i] >> r[i] >> x[i];
+	rep(i,q)
+	{
+		v[l[i]] += x[i];
+		v[r[i]+1] -= x[i];
+	}
+
+	vector<int> cum(n+1);
+	rep(i,1,n)
+	{
+		cum[i] = cum[i - 1] + v[i];
+	}
+	rep(i,1,n-1)
+	{
+		if (cum[i] < cum[i + 1])  cout << "<";
+		if (cum[i] > cum[i + 1])  cout << ">";
+		if (cum[i] == cum[i + 1]) cout << "=";
+	}
+	cout << endl;
+	return 0;
+}
