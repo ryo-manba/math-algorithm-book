89bbd3ac67d2ff51d45b939f4f2c5de859c88ffc
Ryomanba <datemanba3@gmail.com>
2022-06-09 23:32:46 +0900

    自身より小さい隣接頂点の個数

---
+++ ex04/045.cpp
@@ -0,0 +1,62 @@
+// #pragma GCC target ("avx2")
+// #pragma GCC optimization ("O3")
+// #pragma GCC optimization ("unroll-loops")
+#include <bits/stdc++.h>
+#define rep1(n)       for (int rep1_i= 0; rep1_i < n; rep1_i++)
+#define rep2(i, n)    for (int i = 0; i < n; i++)
+#define rep3(i, m, n) for (int i = m; i <= n; i++)
+#define overload3(o1, o2, o3, name, ...) name
+#define rep(...) overload3(__VA_ARGS__, rep3,rep2,rep1)(__VA_ARGS__)
+#define INF ((1<<30)-1)
+#define INFL ((1LL<<62)-1)
+#define MOD 998244353
+using namespace std;
+using ll = long long;
+using ld = long double;
+using pll = pair<ll, ll>;
+struct fast_ios { fast_ios(){ cin.tie(nullptr), ios::sync_with_stdio(false), cout << fixed << setprecision(20); }; } fast_ios_;
+int dx[]={0, 0, -1, 1};
+int dy[]={1, -1, 0, 0};
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+#define debug(var)  do{std::cout << "[L:" << __LINE__ << "] " << #var << " : ";view(var);}while(0)
+template<typename T> void view(T e){std::cout << e << std::endl;}
+template<typename T> void view(const std::vector<T>& v){for(const auto& e : v){ std::cout << e << " "; } std::cout << std::endl;}
+template<typename T> void view(const std::vector<std::vector<T> >& vv){ for(const auto& v : vv){ view(v); } }
+
+vector<int> graph[100010];
+
+int main()
+{
+	int n, m;
+	cin >> n >> m;
+	vector<int> a(m), b(m);
+	rep(i,m) cin >> a[i] >> b[i];
+
+	rep(i,m)
+	{
+		graph[a[i]].push_back(b[i]);
+		graph[b[i]].push_back(a[i]);
+	}
+
+	int ans = 0;
+	rep(i,1,n)
+	{
+		int cnt = 0;
+		for (auto p : graph[i])
+		{
+			if (p < i) {
+				cnt += 1;
+			}
+			if (cnt == 2) {
+				break;
+			}
+		}
+		if (cnt == 1) {
+			ans += 1;
+		}
+	}
+
+	cout << ans << endl;
+	return 0;
+}
