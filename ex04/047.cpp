50e18d610efd5198c9ae8fe64fe3fdd2d0fd612c
Ryomanba <datemanba3@gmail.com>
2022-06-11 19:56:23 +0900

    二部グラフの判定

---
+++ ex04/047.cpp
@@ -0,0 +1,87 @@
+// #pragma GCC target ("avx2")
+// #pragma GCC optimization ("O3")
+// #pragma GCC optimization ("unroll-loops")
+#include <bits/stdc++.h>
+#define rep1(n)       for (int rep1_i= 0; rep1_i < n; rep1_i++)
+#define rep2(i, n)    for (int i = 0; i < n; i++)
+#define rep3(i, m, n) for (int i = m; i <= n; i++)
+#define overload3(o1, o2, o3, name, ...) name
+#define rep(...) overload3(__VA_ARGS__, rep3,rep2,rep1)(__VA_ARGS__)
+#define INF ((1<<30)-1)
+#define INFL ((1LL<<62)-1)
+#define MOD 998244353
+using namespace std;
+using ll = long long;
+using ld = long double;
+using pll = pair<ll, ll>;
+struct fast_ios { fast_ios(){ cin.tie(nullptr), ios::sync_with_stdio(false), cout << fixed << setprecision(20); }; } fast_ios_;
+int dx[]={0, 0, -1, 1};
+int dy[]={1, -1, 0, 0};
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+#define debug(var)  do{std::cout << "[L:" << __LINE__ << "] " << #var << " : ";view(var);}while(0)
+template<typename T> void view(T e){std::cout << e << std::endl;}
+template<typename T> void view(const std::vector<T>& v){for(const auto& e : v){ std::cout << e << " "; } std::cout << std::endl;}
+template<typename T> void view(const std::vector<std::vector<T> >& vv){ for(const auto& v : vv){ view(v); } }
+
+vector<int> g[200200];
+vector<bool> seen(200200, false);
+queue<int> que;
+
+// colorを0, 1で判定する
+vector<int> color(200200, 0);
+
+void solve(int x, int y)
+{
+	if (seen[x])
+	{
+		if (color[x] == color[y])
+		{
+			cout << "No" << endl;
+			exit(0);
+		}
+	}
+	else
+	{
+		seen[x] = true;
+		if (color[y] == 0) color[x] = 1;
+		else color[x] = 0;
+		que.push(x);
+	}
+}
+
+int main()
+{
+	int n, m;
+	cin >> n >> m;
+	vector<int> a(m), b(m);
+	rep(i,m) cin >> a[i] >> b[i];
+
+	rep(i,m)
+	{
+		g[a[i]].push_back(b[i]);
+		g[b[i]].push_back(a[i]);
+	}
+
+	color[a[0]] = 1;
+	rep(i,m)
+	{
+		if (seen[a[i]]) continue;
+
+		seen[a[i]] = true;
+		for (auto p : g[a[i]])
+		{
+			solve(p, a[i]);
+		}
+		while (!que.empty())
+		{
+			for (auto p : g[que.front()])
+			{
+				solve(p, que.front());
+			}
+			que.pop();
+		}
+	}
+	cout << "Yes" << endl;
+	return 0;
+}
